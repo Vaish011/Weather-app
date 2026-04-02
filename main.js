082aba7968314f59092bf65020564a26f8a03a0b
Vaish011 <91137291+Vaish011@users.noreply.github.com>
2026-04-03 00:07:15 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const icon = document.getElementById("weather-icon");
+
+let weather = "Clouds"; // try Rain, Snow, Clear
+
+if (weather === "Clouds") {
+  icon.src = "cloud.png";
+} else if (weather === "Rain") {
+  icon.src = "rain.png";
+} else if (weather === "Snow") {
+  icon.src = "snow.png";
+} else {
+  icon.src = "sun.png";
+}
