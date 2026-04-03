24ba7dffd6d7f593e53a34c7622c9b36e61cae10
Vaish011 <91137291+Vaish011@users.noreply.github.com>
2026-04-03 13:39:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
-alert("JS loaded");
-const icon = document.getElementById("weather-icon");
-
-let weather = "Snow"; // try Rain, Snow, Clear
-
-if (weather === "Clouds") {
-  icon.src = "cloud.png";
-} else if (weather === "Rain") {
-  icon.src = "rain.png";
-} else if (weather === "Snow") {
-  icon.src = "snow.png";
-} else {
-  icon.src = "sun.png";
-}
-async function searchCity() {
+function searchCity() {
     const city = document.getElementById("search-input").value;
 
     const apiKey = "89ad8ff1eb1720de8864d021b2f7d833";
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" 
+                + city + 
+                "&units=metric&appid=" + apiKey;
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
 
-        document.querySelector(".return").style.display = "block"; 
+            // show result section (important if hidden)
+            const box = document.querySelector(".return");
+            if (box) box.style.display = "block";
 
-        document.querySelector(".city-name").innerText = data.name;
-        document.querySelector(".weather-temp").innerText = data.main.temp + "°C";
-
-    } catch (error) {
-        alert("City not found ❌");
-    }
+            // update UI
+            document.querySelector(".city-name").innerText = data.name;
+            document.querySelector(".weather-temp").innerText = data.main.temp + "°C";
+        })
+        .catch(error => {
+            alert("City not found ❌");
+            console.log(error);
+        });
 }
