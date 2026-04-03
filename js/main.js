d9a45f84453408f2ec1c34dd9b57ada5725b776d
Vaish011 <91137291+Vaish011@users.noreply.github.com>
2026-04-03 15:30:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,6 +55,7 @@ const apiKey = "89ad8ff1eb1720de8864d021b2f7d833";
 
 // 🔹 get weather using coordinates
 function getWeatherByCoords(lat, lon) {
+    alert("GPS got: " + lat + ", " + lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
     fetch(url)
