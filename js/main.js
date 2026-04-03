64d5fa36a60daf2a413fc78be9d9fbe6e9865ff9
Vaish011 <91137291+Vaish011@users.noreply.github.com>
2026-04-03 15:58:38 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,10 +58,18 @@ function getWeatherByCoords(lat, lon) {
     alert("GPS got: " + lat + ", " + lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
     alert("URL is: " + url);
-    fetch(url)
+    /*fetch(url)
         .then(res => res.json())
         .then(data => updateUI(data))
-        .catch(() => alert("Fetch failed!"))
+        .catch(() => alert("Fetch failed!"))*/
+
+    fetch(url)
+        .then(res => {
+        alert("Status: " + res.status);
+        return res.json();
+    })
+        .then(data => updateUI(data))
+        .catch((err) => alert("Fetch failed: " + err.message));
 }
 
 // 🔹 get weather using city (for search page)
